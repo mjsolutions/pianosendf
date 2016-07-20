dea0dfd0de9b7b06835fe8ad18c99055c775bdfa
martinalanis <martinalanis.dev@gmail.com>
2016-07-19 21:10:46 -0500

    Inicializacion

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="description" content="">
+    <meta name ="Robots" content="index, follow" />
+    <meta name="author" content="NodoLab">
+    <meta name="COPYRIGHT" content="NodoLab">
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="css/normalize.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+</head>
+<body class="container-fluid">
+
+	<div class="col-md-6 col-center wrap">
+		
+	</div>
+<!-- 	<script src="js/jquery-2.2.0.min.js"></script> -->
+</body>
+</html>
